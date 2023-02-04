4249efec5b9c9fb8468dc11d95b1145cec153eba
Ольга <olga@mail.ru>
2023-02-04 19:22:34 +0300

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Print2DArray(Generate2DArray(3, 4));
+
+//Метод генерирует массив из случайных вещественных чисел:
+double[,] Generate2DArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(-99, 100) / 10.0;
+        }
+    }
+    return result;
+}
+
+//Метод выводит в консоль заданный массив:
+void Print2DArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
