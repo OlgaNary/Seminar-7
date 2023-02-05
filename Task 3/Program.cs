7dac7274130e690a9a678cf2ba059b6c00891a9f
Ольга <olga@mail.ru>
2023-02-05 14:25:03 +0300

    Task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+var myArr = Generate2DArray(3, 4);
+Print2DArray(myArr);
+System.Console.Write("Среднее арифметическое каждого столбца: ");
+GetAverage(myArr);
+
+//Метод находит среднее арифметическое элементов в каждом столбце:
+void GetAverage(int[,] arr)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        Console.Write($"{sum / arr.GetLength(0)} ");
+    }
+}
+
+//Метод генерирует массив из случайных целых чисел:
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+//Метод выводит на печать массив:
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
